3e1b5f5e041a177d448c853c75439799f3245e83
Maksim Terentyev <terentyev@gmail.com>
2016-12-29 19:10:08 -0800

    Adds OpenShift integration.

--- frontend/src/app/components/Bye.tsx
+++ frontend/src/app/components/Bye.tsx
@@ -7,8 +7,7 @@ export interface ByeProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export class Bye extends React.Component<ByeProps, undefined> {
     render() {
-        return
-        <h1 className="container">
+        return <h1 className="container">
             Good bye from {this.props.compiler} and {this.props.framework}!
         </h1>;
     }
